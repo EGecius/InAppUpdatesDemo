2c658603530a9cbe6e4d9e6913d788bd9aa62a45
Egidjus Gecius <e.gecius@gmail.com>
2020-03-10 06:30:14 +0000

    add methods to request an updae

--- app/src/main/java/uk/com/egecius/inappupdatesdemo/MainActivity.kt
+++ app/src/main/java/uk/com/egecius/inappupdatesdemo/MainActivity.kt
@@ -1,12 +1,70 @@
 package uk.com.egecius.inappupdatesdemo
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.play.core.appupdate.AppUpdateInfo
+import com.google.android.play.core.appupdate.AppUpdateManager
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.model.AppUpdateType.IMMEDIATE
+import com.google.android.play.core.install.model.UpdateAvailability.UPDATE_AVAILABLE
 
 class MainActivity : AppCompatActivity() {
+
+    // Creates instance of the manager.
+    private val appUpdateManager: AppUpdateManager by lazy {
+        AppUpdateManagerFactory.create(this)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        checkIfUpdateAvailable()
+    }
+
+    private fun checkIfUpdateAvailable() {
+        // Returns an intent object that you use to check for an update.
+        val appUpdateInfoTask = appUpdateManager.appUpdateInfo
+
+        // Checks that the platform will allow the specified type of update.
+        appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.updateAvailability() == UPDATE_AVAILABLE
+                // For a flexible update, use AppUpdateType.FLEXIBLE
+                && appUpdateInfo.isUpdateTypeAllowed(IMMEDIATE)
+            ) {
+                requestUpdate(appUpdateInfo)
+            }
+        }
+    }
+
+    private fun requestUpdate(appUpdateInfo: AppUpdateInfo) {
+        appUpdateManager.startUpdateFlowForResult(
+            // Pass the intent that is returned by 'getAppUpdateInfo()'.
+            appUpdateInfo,
+            // Or 'AppUpdateType.FLEXIBLE' for flexible updates.
+            IMMEDIATE,
+            // The current activity making the update request.
+            this,
+            // Include a request code to later monitor this update request.
+            MY_REQUEST_CODE
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == MY_REQUEST_CODE) {
+            if (resultCode != RESULT_OK) {
+                Log.v("Eg:MainActivity:60", "onActivityResult() Update flow failed! Result code: $resultCode")
+                // If the update is cancelled or fails,
+                // you can request to start the update again.
+            }
+        }
+    }
+
+    companion object {
+
+    	const val MY_REQUEST_CODE = 813132
+
     }
 }
